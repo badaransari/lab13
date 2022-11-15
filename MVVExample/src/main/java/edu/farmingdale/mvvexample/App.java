adebc818a4d0f02829d721447e54bfe0e93ede12
badaransari <badaransari@137.125.147.24>
2022-11-14 23:54:30 -0500

    lab updated

---
+++ MVVExample/src/main/java/edu/farmingdale/mvvexample/App.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.farmingdale.mvvexample;
+
+/**
+ *
+ * @author badaransari
+ */
+
+
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.auth.FirebaseAuth;
+import com.mycompany.mvvmexample.FirestoreContext;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    public static Firestore fstore;
+    public static FirebaseAuth fauth;
+    public static Scene scene;
+    private final FirestoreContext contxtFirebase = new FirestoreContext();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        fstore = contxtFirebase.firebase();
+        fauth = FirebaseAuth.getInstance();
+        scene = new Scene(loadFXML("AccessFBView.fxml"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml ));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+
+}
+
